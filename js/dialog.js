8dd4574ba4ce4d0c787ac464152425c51cf9361f
Georg <georgiy.nikitin@gmail.com>
2020-02-05 16:31:04 +0300

    Перенос в js/dialog.js диалога с user

--- js/dialog.js
+++ js/dialog.js
@@ -1,13 +1,6 @@
 'use strict';
 
-// Перетаскивание диалога из демки
 (function () {
-  // Задание 4
-  // Нажатие на элемент .setup-open удаляет класс hidden
-  // у блока setup. Нажатие на элемент .setup-close, расположенный
-  // внутри блока setup возвращает ему класс hidden.
-
-  /*
   var WIZARD_COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb (0, 0, 0)'];
   var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
   var WIZARD_FIREBALLS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
@@ -19,9 +12,7 @@
   var setupWizardEyes = setup.querySelector('.wizard-eyes');
   var setupWizardFireball = setup.querySelector('.setup-fireball-wrap');
   var userNameInput = setup.querySelector('.setup-user-name');
-  */
 
-  /*
   var onPopupEscPress = function (evt) {
     if (evt.target !== userNameInput) {
       window.keyCheck.isEscEvent(evt, closePopup);
@@ -59,7 +50,6 @@
   window.colorSize(WIZARD_COATS, setupWizardCoat);
   window.colorSize(WIZARD_EYES, setupWizardEyes);
   window.colorSize(WIZARD_FIREBALLS, setupWizardFireball);
-  */
 
 /*
   var setupDialogElement = document.querySelector('.setup');
--- js/setup.js
+++ js/setup.js
@@ -6,24 +6,12 @@
   var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
   var WIZARD_COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb (0, 0, 0)'];
   var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
-  var WIZARD_FIREBALLS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
   var wizards = [];
 
   var similarListElement = document.querySelector('.setup').querySelector('.setup-similar-list');
   var similarWizardTemplate = document.querySelector('#similar-wizard-template')
       .content
       .querySelector('.setup-similar-item');
-  /*
-  // Показать элемент, убрав класс  'hidden'
-  var showElement = function (showClass) {
-    document.querySelector(showClass).classList.remove('hidden');
-  };
-
-  // Спрятать элемент, добавив класс  'hidden'
-  var hideElement = function (hideClass) {
-    document.querySelector(hideClass).classList.add('hidden');
-  };
-  */
 
   // Выдает один рандомный элемент из массива
   var getRandomElement = function (arr) {
@@ -61,61 +49,9 @@
     similarListElement.appendChild(fragment);
   };
 
-  // showElement('.setup');
   createArrayWizards();
   renderWizards();
   window.showElement('.setup-similar');
-
-  // Задание 4
-  // Нажатие на элемент .setup-open удаляет класс hidden
-  // у блока setup. Нажатие на элемент .setup-close, расположенный
-  // внутри блока setup возвращает ему класс hidden.
-  var setup = document.querySelector('.setup');
-  var setupOpen = document.querySelector('.setup-open');
-  var setupClose = setup.querySelector('.setup-close');
-
-  var setupWizardCoat = setup.querySelector('.wizard-coat');
-  var setupWizardEyes = setup.querySelector('.wizard-eyes');
-  var setupWizardFireball = setup.querySelector('.setup-fireball-wrap');
-  var userNameInput = setup.querySelector('.setup-user-name');
-
-  var onPopupEscPress = function (evt) {
-    if (evt.target !== userNameInput) {
-      window.keyCheck.isEscEvent(evt, closePopup);
-    }
-  };
-
-  // Открывает popup по клавише
-  var openPopup = function () {
-    window.showElement('.setup');
-    document.addEventListener('keydown', onPopupEscPress);
-  };
-
-  // Закрывает popup по клавише
-  var closePopup = function () {
-    window.hideElement('.setup');
-    document.removeEventListener('keydown', onPopupEscPress);
-  };
-
-  setupOpen.addEventListener('click', function () {
-    openPopup();
-  });
-
-  setupOpen.addEventListener('keydown', function (evt) {
-    window.keyCheck.isEnterEvent(evt, openPopup);
-  });
-
-  setupClose.addEventListener('click', function () {
-    closePopup();
-  });
-
-  setupClose.addEventListener('keydown', function (evt) {
-    window.keyCheck.isEnterEvent(evt, closePopup);
-  });
-
-  window.colorSize(WIZARD_COATS, setupWizardCoat);
-  window.colorSize(WIZARD_EYES, setupWizardEyes);
-  window.colorSize(WIZARD_FIREBALLS, setupWizardFireball);
 
 })();
 
