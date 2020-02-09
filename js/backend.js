4847e50cecfec2a2a642f3be8377074e81b495bd
Georg <georgiy.nikitin@gmail.com>
2020-02-09 18:40:35 +0300

    Добавление в js/utils.js доп функций

--- js/backend.js
+++ js/backend.js
@@ -1,33 +1,123 @@
 'use strict';
 
 (function () {
-  var URL = 'https://js.dump.academy/code-and-magick/data';
   var StatusCode = {
     OK: 200
   };
   var TIMEOUT_IN_MS = 10000;
 
-  window.load = function (onLoad, onError) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
+  window.backend = {
+    // Выгрузка с сервера данных
+    load: function (onLoad, onError) {
+      var URL = 'https://js.dump.academy/code-and-magick/data';
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
 
-    xhr.addEventListener('load', function () {
-      if (xhr.status === StatusCode.OK) {
-        onLoad(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения. Нет подключения к Интернету');
-    });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
+      xhr.addEventListener('load', function () {
+        if (xhr.status === StatusCode.OK) {
+          onLoad(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения. Нет подключения к Интернету');
+      });
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
 
-    xhr.timeout = TIMEOUT_IN_MS;
+      xhr.timeout = TIMEOUT_IN_MS;
 
-    xhr.open('GET', URL);
-    xhr.send();
+      xhr.open('GET', URL);
+      xhr.send();
+    },
+    // Отправка формы
+    save: function (data, onLoadForm) {
+      var URL = 'https://js.dump.academy/code-and-magick';
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        onLoadForm(xhr.response);
+      });
+
+      xhr.open('POST', URL);
+      xhr.send(data);
+    }
+
+
   };
 })();
+/*
+var form = document.querySelector('.settup-wizard-form');
+  form.addEventListener('submit', function (evt) {
+    window.backend.save(new FormData(form), function () {
+      window.utils.hideElement('.setup-similar');
+    });
+    evt.preventDefault();
+  });
+*/
+
+/*
+'use strict';
+
+(function () {
+  var WIZARD_NUMBER = 4;
+
+  var wizards;
+
+  var similarListElement = document.querySelector('.setup').querySelector('.setup-similar-list');
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+      .content
+      .querySelector('.setup-similar-item');
+
+  // Клонирует по шаблону Wizard и задает имя параметры name, coatColor и eyesColor
+  var renderWizard = function (wizard) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
+
+    return wizardElement;
+  };
+
+  // Выводит в разметку WIZARD_NUMBER полученных с сервера wizardLoad рандомно
+  var onLoad = function (wizardsLoad) {
+    var fragment = document.createDocumentFragment();
+    wizards = window.utils.getRandomArray(wizardsLoad, WIZARD_NUMBER);
+    for (var i = 0; i < WIZARD_NUMBER; i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
+    }
+    similarListElement.appendChild(fragment);
+    window.utils.showElement('.setup-similar');
+  };
+
+  var onError = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.top = '40px';
+
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.backend.load(onLoad, onError);
+
+  var form = document.querySelector('.settup-wizard-form');
+  form.addEventListener('submit', function (evt) {
+    window.backend.save(new FormData(form), function () {
+      window.utils.hideElement('.setup-similar');
+    });
+    evt.preventDefault();
+  });
+
+
+})();
+*/
--- js/utils.js
+++ js/utils.js
@@ -24,6 +24,37 @@
     // Показать элемент, убрав класс  'hidden'
     showElement: function (showClass) {
       document.querySelector(showClass).classList.remove('hidden');
+    },
+
+    // Выдает рандомное число в диапозоне от minNumber до maxNumber
+    getRandomRange: function (minNumber, maxNumber) {
+      return Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+    },
+    // Выдает на основе входящего массива один рандомный элемент массива
+    getRandomElement: function (arr) {
+      var numberRandom = window.utils.getRandomRange(0, arr.length - 1);
+      return arr[numberRandom];
+    },
+    // Выдает на основе входящего массива массив с заданным или рандомным(если не задано) кол-вом элементов
+    getRandomArray: function (arr, numberRandom) {
+      if (!numberRandom) {
+        numberRandom = window.utils.getRandomRange(0, arr.length);
+      }
+      var arrClon = arr.slice();
+      var arrNew = [];
+      var numberArrRandom;
+
+      for (var j = 0; j < numberRandom; j++) {
+        numberArrRandom = window.utils.getRandomRange(0, arrClon.length - 1);
+        arrNew[j] = arrClon[numberArrRandom];
+        arrClon.splice(numberArrRandom, 1);
+      }
+      return arrNew;
+    },
+    // Выдает на основе входящего объекта рандомный элемент(не ключ!)
+    getRandomObject: function (obj) {
+      var array = Object.keys(obj);
+      return window.utils.getRandomElement(array);
     }
   };
 })();
