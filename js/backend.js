7816fcbf020e9986b9638317cb76ce57b4afb214
Georg <georgiy.nikitin@gmail.com>
2020-02-10 14:05:00 +0300

    Вынес повторяющий код при передачи данных в prepareLoad

--- js/backend.js
+++ js/backend.js
@@ -6,55 +6,40 @@
   };
   var TIMEOUT_IN_MS = 10000;
 
+  // Подготавливает данные к загрузке или отправке и проверяет callback
+  var prepareLoad = function (onLoad, onError) {
+    xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
+      if (xhr.status === StatusCode.OK) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения. Нет подключения к Интернету');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = TIMEOUT_IN_MS;
+  };
+
+  var xhr;
+
   window.backend = {
     // Выгрузка с сервера данных
-    load: function (onLoad, onError) {
-      var URL = 'https://js.dump.academy/code-and-magick/data';
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === StatusCode.OK) {
-          onLoad(xhr.response);
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения. Нет подключения к Интернету');
-      });
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-      });
-
-      xhr.timeout = TIMEOUT_IN_MS;
+    load: function (URL, onLoad, onError) {
+      prepareLoad(onLoad, onError);
 
       xhr.open('GET', URL);
       xhr.send();
     },
     // Отправка формы
-    save: function (data, onLoadForm, onError) {
-      var URL = 'https://js.dump.academy/code-and-magick';
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function () {
-        onLoadForm(xhr.response);
-      });
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === StatusCode.OK) {
-          onLoadForm(xhr.response);
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения. Нет подключения к Интернету');
-      });
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-      });
+    save: function (URL, data, onLoadForm, onError) {
+      prepareLoad(onLoadForm, onError);
 
       xhr.open('POST', URL);
       xhr.send(data);
