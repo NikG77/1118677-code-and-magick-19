ab134d735cd0d45db48eb186d1aad23c01edd1bf
Georg <georgiy.nikitin@gmail.com>
2020-02-16 17:37:57 +0300

    Выношу функции рисования wizards в js/render.js

---
+++ js/render.js
@@ -0,0 +1,35 @@
+'use strict';
+
+(function () {
+  var WIZARD_NUMBER = 4;
+  var similarListElement = document.querySelector('.setup').querySelector('.setup-similar-list');
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+      .content
+      .querySelector('.setup-similar-item');
+
+
+  // Клонирует по шаблону Wizard и задает имя параметры name, coatColor и eyesColor
+  var renderWizard = function (wizard) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
+
+    return wizardElement;
+  };
+
+  // Выводит в разметку весь массив полученных с сервера wizard
+  window.render = function (wizardsOnload) {
+    var wizards = window.utils.getAnyArray(wizardsOnload, WIZARD_NUMBER);
+    var fragment = document.createDocumentFragment();
+    var takeNumber = wizardsOnload.length > 4 ? 4 : wizardsOnload.length;
+    similarListElement.innerHTML = '';
+    for (var i = 0; i < takeNumber; i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
+    }
+    similarListElement.appendChild(fragment);
+    window.utils.showElement('.setup-similar');
+  };
+
+})();
