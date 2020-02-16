4265ae9a030d7d94b053bf965bfd401e500bbb43
Georg <georgiy.nikitin@gmail.com>
2020-02-16 23:00:50 +0300

    Исправил ошибку из-за лишнего пробела в данных colorCoat

--- js/setup.js
+++ js/setup.js
@@ -5,9 +5,17 @@
   var URL_FORM = 'https://js.dump.academy/code-and-magick';
   var wizards = [];
 
+  var WIZARD_COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+  var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+  var WIZARD_FIREBALLS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
   var setup = document.querySelector('.setup');
+
+  var setupWizardCoat = setup.querySelector('.wizard-coat');
+  var setupWizardEyes = setup.querySelector('.wizard-eyes');
+  var setupWizardFireball = setup.querySelector('.setup-fireball-wrap');
   var setupWizardCoatInput = setup.querySelector('input[name="coat-color"]');
   var setupWizardEyesInput = setup.querySelector('input[name="eyes-color"]');
+  var setupWizardFireballInput = setup.querySelector('input[name="fireball-color"]');
 
   var getRank = function (wizard) {
     var rank = 0;
@@ -67,6 +75,11 @@
     window.utils.hideElement('.setup');
     submitButton.textContent = 'Сохранить';
   };
+
+  window.colorSize.colorChange(WIZARD_COATS, setupWizardCoat, setupWizardCoatInput);
+  window.colorSize.colorChange(WIZARD_EYES, setupWizardEyes, setupWizardEyesInput);
+
+  window.colorSize.colorChange(WIZARD_FIREBALLS, setupWizardFireball, setupWizardFireballInput);
 
   var form = document.querySelector('.setup-wizard-form');
   var submitButton = form.querySelector('.setup-submit');
